d39fce6388108f0964debf58aa2d7b403306a5ae
luispfcanales <luispfcanales@gmail.com>
2023-12-26 23:21:00 -0500

    feature: deleted files unused

--- server.go
+++ server.go
@@ -22,7 +22,6 @@ func NewServer(e *fiber.App) *server {
 func (s *server) Run() error {
 	log.SetFlags(log.Lshortfile | log.Ldate)
 	ConfigRoutes(s.engine)
-	//return s.engine.ListenTLS(":443", "server.crt", "server.key")
 	return s.engine.Listen(s.getPort())
 }
 
